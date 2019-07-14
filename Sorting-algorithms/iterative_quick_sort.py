e22adf732312fad55a2b8206224bfd5d4f2d0c81
gilfoyle97 <35783570+edoardottt@users.noreply.github.com>
2019-07-14 09:34:04 +0200

    Add files via upload
    Added sorting algortihms

---
+++ Sorting-algorithms/iterative_quick_sort.py
@@ -0,0 +1,17 @@
+def partition(arr, low, high): 
+	i = (low - 1)		 # index of smaller element 
+	pivot = arr[high]	 # pivot 
+
+	for j in range(low, high): 
+		if arr[j] <= pivot: 
+			i += 1
+			arr[i], arr[j] = arr[j], arr[i] 
+
+	arr[i + 1], arr[high] = arr[high], arr[i + 1] 
+	return (i + 1) 
+
+def quickSort(arr, low, high): 
+	if low < high: 
+		pi = partition(arr, low, high) 
+		quickSort(arr, low, pi-1) 
+		quickSort(arr, pi + 1, high) 
---
+++ Sorting-algorithms/recursive_bubble_sort.py
@@ -0,0 +1,11 @@
+def bubble_sort(listres): 
+	for i, num in enumerate(listres): 
+		try: 
+			if listres[i+1] < num: 
+				listres[i] = listres[i+1] 
+				listres[i+1] = num 
+				bubble_sort(listres) 
+		except IndexError: 
+			pass
+	return listres
+
